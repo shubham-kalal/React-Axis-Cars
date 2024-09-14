80074f009baf63436e7cd317acac12cd7b09f028
shubham-kalal <shubhamkalal2611@gmail.com>
2024-09-14 14:23:21 +0530

    responsive changes

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,23 +1,14 @@
 import React from "react";
 import { GoArrowUpRight } from "react-icons/go";
+import "../Style/Button.css"
 
 
-const button = {
-  padding: "22.35px 27.94px",
-  fontWeight: "300",
-  fontSize: "26.82px",
-  lineHeight: "26.94px",
-  textAlign: "center",
-  color: "white",
-  background: "#FFFFFF26",
-  border: "1px solid white",
-};
 
 
 
 const Button = () => {
   return (
-    <button className="btn" style={button}>
+    <button className="btn" >
       <a href="">
         Order Now
         <GoArrowUpRight />
--- src/Components/Navbtn.jsx
+++ src/Components/Navbtn.jsx
@@ -1,37 +1,16 @@
-import react from "react";
+import React from "react";
 import { GoArrowUpRight } from "react-icons/go";
+import "../Style/Navbtn.css";
 
-
-
-const navbutton = {
-    background: 'transparent',
-    padding:'12px 36px',
-    border:".5px solid white",
-    fontSize:'14px',
-    fontWeight:'300',
-    lineHeight:'12.5px',
-    display:'flex',
-    flexDirection:'collum',
-    alignItems:'center',
-    textTransform:'capitalize'
-
-
-
+const Navbtn = () => {
+  return (
+    <button className="nav-button">
+      <a href="">
+        contact us
+        <GoArrowUpRight />
+      </a>
+    </button>
+  );
 };
-
-const Navbtn = ()=>{
-    return(
-        <>
-        <button style={navbutton}>
-            <a href="">
-                
-                contact us
-                <GoArrowUpRight />
-            </a>
-        </button>
-        </>
-    )
-}
-
 
 export default Navbtn;
